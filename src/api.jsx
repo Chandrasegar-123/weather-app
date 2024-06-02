1b84345bcd104691038accfe993f81ac88bb9810
Mr. J.CHANDRASEGAR <chandramayou067@gmail.com>
2024-06-02 20:22:43 +0530

    cities fetch done

---
+++ src/api.jsx
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'cd5ad99bcdmshbedffef21dae4aep15a310jsn6ac9b8240db7',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
