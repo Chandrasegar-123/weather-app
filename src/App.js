b7673bfa9576a1869c606caaf3a111389364965a
Mr. J.CHANDRASEGAR <chandramayou067@gmail.com>
2024-06-06 12:12:06 +0530

    all set

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Search from './components/search/search';
 import CurrentWeather from './components/current-weather/current-weather';
+import Forecast from './components/forecast/forecast';
 import { WEATHER_API_KEY, WEATHER_API_URL, FORECAST_API_URL} from './api';
 import { useState } from 'react';
 
@@ -33,6 +34,7 @@ function App() {
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
      {currentWeather && <CurrentWeather data= {currentWeather} />}
+     {forecast && <Forecast data= {forecast} />}
     </div>
   );
 }
